8fcc85f81524b185b4b5802302158b75fc12e39c
Тимур <mihailintimur@gmail.com>
2022-10-22 11:19:01 +0300

    yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

---
+++ main.py
@@ -0,0 +1,32 @@
+score = 0
+
+print('Что такое сокет?')
+print('a) Разьем для процессора')
+print('b) Блок питания')
+print('c) Разьем для оперативки')
+print('d) чебурек')
+
+
+answer = input('Выберите ответ: ')
+if answer == 'a':
+    print('Правильно!')
+
+print('Сколько лап у котов на руках?')
+print('a) 4')
+print('b) 3')
+print('c) У котов нету лап на руках')
+print('d) 5')
+
+answer = input('Выберите ответ: ')
+if answer == 'c':
+    print('Правильно!')
+
+print('ты??')
+print('a) ты')
+print('b) тыя')
+print('c) ты')
+print('d) ты')
+
+answer = input('Выберите ответ: ')
+if answer == 'b':
+    print('Правильно!')
